446ee44bf7c688daf080785194b1e40ed016d8a0
Adolfo Guimarães <adolfoguimaraes@gmail.com>
2019-02-04 19:24:25 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from Interpreter import Interpreter
+
+
+def main():
+
+    while True:
+
+        try:
+
+            text = input("calc> ")
+        except EOFError:
+            break
+        
+        if not text:
+            continue
+
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+
+        print(result)
+
+if __name__ == "__main__":
+    main()
